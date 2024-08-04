4a33fc113c08126a8315878d22fbc747d595c169
badMeme <noveladdress@gmail.com>
2024-08-04 01:47:08 -0500

    dungeon procedural generation exists but the room connecting portion is bugged. Will fix in the morning.

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ from tcod import libtcodpy # <- For refactor, sys warnings
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
+# from game_map import GameMap
 # from actions import EscapeAction, MovementAction
 from input_handlers import EventHandler# handle_keys
+from procgen import generate_dungeon
 
 
 DATA_FOLDER = 'data'
@@ -29,6 +30,10 @@ def main() -> None:
     map_width = 80
     map_height = 45
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     tileset = tcod.tileset.load_tilesheet(FONT_FILE, 32, 8, tcod.tileset.CHARMAP_TCOD)
     
     event_handler = EventHandler()
@@ -37,7 +42,15 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height/2), "@", (255,255,0))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    # game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
