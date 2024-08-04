c015ac881d25e38adbabd77d00b37219d0b73a11
badMeme <noveladdress@gmail.com>
2024-08-04 16:04:37 -0500

    added FOV functionality

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -13,6 +14,7 @@ class Engine:
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
+        self.update_fov()
 
     def handle_events (self, events: Iterable[Any]) -> None:
         for event in events: 
@@ -21,6 +23,18 @@ class Engine:
                 continue
             action.perform(self, self.player)
                 
+            self.update_fov() 
+            #update the FOV before the player's next action
+
+    def update_fov(self) -> None:
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles['transparent'],
+            (self.player.x, self.player.y), 
+            radius = 8,
+        ) 
+        #if a tile is visible, add to explored
+        self.game_map.explored |= self.game_map.visible   
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         for entity in self.entities:
