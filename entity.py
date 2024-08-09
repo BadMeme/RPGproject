6e2fb70faa99f96ad8aa39b40c953bb30b366d9b
badMeme <noveladdress@gmail.com>
2024-08-09 02:32:41 -0500

    refactor for increased functionality. Render function broken, need to debug

--- entity.py
+++ entity.py
@@ -1,16 +1,21 @@
 from __future__ import annotations
 
-from typing import Tuple, TypeVar, TYPE_CHECKING
+from typing import Optional, Tuple, TypeVar, TYPE_CHECKING
 import copy
 
 if TYPE_CHECKING:
     from game_map import GameMap
 T = TypeVar('T', bound='Entity')
 
+
+
 class Entity:
     # Generic object class encompassing player, enemy, item, etc.
+    gamemap: GameMap
+
     def __init__(
             self, 
+            gamemap: Optional[GameMap] = None,
             x: int = 0, 
             y: int = 0, 
             char: str = '?', # = '?'
@@ -24,18 +29,31 @@ class Entity:
         self.color = color
         self.name = name
         self.blocks_movement = blocks_movement
+        if gamemap :
+            # if gamemap isnt provided now then it will be set later.
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def spawn(self: T, gamemap: GameMap, x:int, y:int) -> T:
         """Spawn a copy of this instance at the given lcoation."""
         clone = copy.deepcopy(self)
         clone.x = x
         clone.y = y
+        clone.gamemap = gamemap
         gamemap.entities.add(clone)
         return clone
+
+    def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
+        """Place this entity at a new location. handles moving across gamemaps."""
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"): #possibly uninitialized.
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def move(self, dx: int, dy: int) -> None:
         # move the entity by a given amount
         self.x += dx
         self.y += dy
-
-  
--- main.py
+++ main.py
@@ -38,23 +38,22 @@ def main() -> None:
     max_monsters_per_room = 2
 
     tileset = tcod.tileset.load_tilesheet(FONT_FILE, 32, 8, tcod.tileset.CHARMAP_TCOD)
-    
-    event_handler = EventHandler()
 
     # player = Entity(int(screen_width / 2), int(screen_height/2), "@", (255,255,255))
     player = copy.deepcopy(gameobjects.player)
+    engine = Engine(player=player)
     
-    game_map = generate_dungeon(
+    engine.game_map = generate_dungeon(
         max_rooms=max_rooms,
         room_min_size=room_min_size,
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
         max_monsters_per_room=max_monsters_per_room,
-        player=player
+        engine=engine,
     )
 
-    engine = Engine(event_handler=event_handler, game_map=game_map, player=player)
+    engine.update_fov()
 
     with tcod.context.new_terminal(
         screen_width, 
@@ -65,9 +64,7 @@ def main() -> None:
     ) as context :
         root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True: 
-            engine.render(console=root_console, context=context)
-            events = tcod.event.wait()
-            engine.handle_events(events)
+            engine.event_handler.handle_events()
 
 if __name__ == "__main__" :
     main()
